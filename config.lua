a639861244e0ab65561d2443041f926e10b638e7
EJJ04 <144691106+EJJ04@users.noreply.github.com>
2024-09-07 20:09:24 +0200

    Add files via upload

--- config.lua
+++ config.lua
@@ -15,4 +15,12 @@ Config.SignHashes = {
 Config.Strings = {
     title = 'Steal Sign',
     progressbartitle = 'Stealing sign...',
+    progressCirclelabel = 'Placing sign...',
+    TextUI = '**Controls**  \n [Q] - Rotate Left  \n [E] - Rotate Right  \n [W] - Forward  \n [A] - Left  \n [S] - Backward  \n [D] - Right  \n [X] - Cancel  \n [Space] - Confirm',
 }
+
+Config.CanPlaceSign = true
+Config.maxDistance = 5.0
+Config.moveSpeed = 0.1
+Config.rotationSpeed = 2.0
+Config.progressCircleduration = 1500
